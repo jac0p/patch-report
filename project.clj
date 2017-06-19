b5724eb7c4da6604f6fce56fe469c43ed96d32e5
jac0p <roland.vrg87@gmail.com>
2017-06-19 15:31:03 -0400

    aot included

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :main patch-report.core
-  ;:aot [patch-report.core]
+  :aot [patch-report.core]
   :uberjar-exclusions [#"user.clj"]
   :uberjar-name "patch-report-agent.jar"
   :uberjar {:aot :all}
